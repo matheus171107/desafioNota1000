eaa02e310e9434ca37f1bfd0a8e1f4702b113c22
matheus171107 <matheus07sza@gmail.com>
2025-04-24 15:52:45 -0300

    TelaHome

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,26 @@
 import Header from '../components/Header'  
 import "./home.css"
+import logo from '../assets/logoHeader.png'
+import home from '../assets/Home.png'
   
 function Home() {
   return (
     <>
     <main id="mainHome">
-      <h1>ENEM</h1>
+      <div id="menuLateral">
+        <img src={logo} alt="" />
+        <ul>
+          <li><img src={home} alt="" />Home</li>
+          <li>Desempenho</li>
+          <li>Video Aulas</li>
+          <li>Créditos</li>
+          <li>Sair</li>
+        </ul>
+      </div>
+      <div id="conteudo">
+        <h1>ENEM</h1>
+        <hr/>
+      </div>
     </main>
     </>
   );
